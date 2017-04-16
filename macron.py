e71034cea9727250dd9eb08dbe65f7d0a0d590a1
greuet <aureliengreuet@gmail.com>
2017-04-17 00:10:15 +0200

    listWords on 80 char max lines

--- macron.py
+++ macron.py
@@ -4,7 +4,13 @@ from random import randint
 
 nbWords = 8
 
-listWords = ["projet","rassembler","avancer","réconcilier","mutation","réel","ambition","renouvellement","progressiste","égalitarisme","partenariat","esprit d'initiative","innovation","pénibilité","souplesse","libérer le travail","je n'ai pas peur de le dire","je veux être clair","modernité","charte","droits et devoirs","numérique","responsabilité","combat","pardon de vous le dire","efficace"]
+listWords =
+["projet","rassembler","avancer","réconcilier","mutation","réel","ambition",
+"renouvellement","progressiste","égalitarisme","partenariat",
+"esprit d'initiative","innovation","pénibilité","souplesse",
+"libérer le travail","je n'ai pas peur de le dire","je veux être clair",
+"modernité","charte","droits et devoirs","numérique","responsabilité","combat",
+"pardon de vous le dire","efficace"]
 
 selectedIndices = [randint(0,len(listWords)-1)]
 i = 0
